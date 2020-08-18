a35278e3bb3c70c8805992d7f349e30787df7a19
Deepalimane25 <manedeepali98@gmail.com>
2020-08-18 16:50:22 +0530

    updated

--- FoodieApp/src/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import userReducer from './cuisines/userReducer'
-
-const rootReducer = combineReducers({
-  user: userReducer
-})
-
-export default rootReducer
